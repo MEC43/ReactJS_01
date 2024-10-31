a654624c99368a5ded6f5dfa6dbc988258c8c46b
MEC43 <myung0403@gmail.com>
2024-10-31 11:53:33 +0900

    ~#6.1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import Button from './Button';
-import styles from './App.module.css'
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(0)
+  const onClick = () => {
+    setCounter(prev => prev + 1)
+  }
+
+  console.log('계속 동작됨')
+  useEffect(() => {
+    console.log('CALL THE API')
+  }, []);
+
+
   return (
     <div className="App">
-      <h1 className={styles.title}>어서와 리액트는 처음이지?</h1>
-      <Button text={'계속'} />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>클릭하세요</button>
     </div>
   );
 }
