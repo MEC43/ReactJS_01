107fb789b00f6c3ffcf448579f3dcb92f91aa5d2
MEC43 <myung0403@gmail.com>
2024-11-01 10:39:38 +0900

    ~#6.3

--- src/App.js
+++ src/App.js
@@ -2,18 +2,35 @@ import { useEffect, useState } from 'react';
 
 function App() {
   const [counter, setCounter] = useState(0)
-  const onClick = () => {
-    setCounter(prev => prev + 1)
-  }
+  const [keyword, setKeyword] = useState('')
+  const onClick = () => setCounter(prev => prev + 1)
+  const onChange = (e) => setKeyword(e.target.value)
 
-  console.log('계속 동작됨')
   useEffect(() => {
-    console.log('CALL THE API')
+    console.log('한번만 동작함')
   }, []);
 
+  useEffect(() => {
+    if (keyword !== '' && keyword.length > 5) {
+      console.log('keyword가 변할때만 동작함')
+    }
+  }, [keyword])
+
+  useEffect(() => {
+    console.log('counter가 변할때만 동작함');
+  }, [counter])
+
+  useEffect(() => {
+    console.log('counter와 keyword가 변할때 동작함');
+  }, [keyword, counter])
 
   return (
     <div className="App">
+      <input
+        type='text'
+        placeholder='검색은 여기서...'
+        onChange={onChange}
+        value={keyword} />
       <h1>{counter}</h1>
       <button onClick={onClick}>클릭하세요</button>
     </div>
