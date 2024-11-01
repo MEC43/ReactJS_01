9533112458b798cc06e3af90c94d0397ca54063f
MEC43 <myung0403@gmail.com>
2024-11-01 11:08:21 +0900

    ~#6.4

--- src/App.js
+++ src/App.js
@@ -1,38 +1,31 @@
 import { useEffect, useState } from 'react';
 
+function Hello() {
+
+
+  // useEffect(function () {
+  //   console.log('안녕~ :)');
+  //   return function () {
+  //     console.log('잘가~ :<');
+  //   }
+  // }, [])
+
+  useEffect(() => {
+    console.log('안녕~ :)');
+    return () => console.log('잘가~ :<')
+  }, [])
+
+  return <h1>안냥~</h1>
+}
+
 function App() {
-  const [counter, setCounter] = useState(0)
-  const [keyword, setKeyword] = useState('')
-  const onClick = () => setCounter(prev => prev + 1)
-  const onChange = (e) => setKeyword(e.target.value)
-
-  useEffect(() => {
-    console.log('한번만 동작함')
-  }, []);
-
-  useEffect(() => {
-    if (keyword !== '' && keyword.length > 5) {
-      console.log('keyword가 변할때만 동작함')
-    }
-  }, [keyword])
-
-  useEffect(() => {
-    console.log('counter가 변할때만 동작함');
-  }, [counter])
-
-  useEffect(() => {
-    console.log('counter와 keyword가 변할때 동작함');
-  }, [keyword, counter])
+  const [showing, setShowing] = useState(false)
+  const onClick = () => setShowing(prev => !prev)
 
   return (
     <div className="App">
-      <input
-        type='text'
-        placeholder='검색은 여기서...'
-        onChange={onChange}
-        value={keyword} />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>클릭하세요</button>
+      <button onClick={onClick}>{showing ? '숨김' : '보기'}</button>
+      {showing ? <Hello /> : null}
     </div>
   );
 }
